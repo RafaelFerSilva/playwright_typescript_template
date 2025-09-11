ccd85201a6667699c36e2a4e3a9d016bebc86b30
RafaelFerSilva <todosomtodavoz@gmail.com>
2025-09-11 08:09:09 -0300

    Create fixture and test to database connection

---
+++ src/fixtures/dbAdapter.ts
@@ -0,0 +1,25 @@
+import { MySQLAdapter } from '@framework/adapters/database/MySQLAdapter';
+import { test as base } from '@playwright/test';
+
+const dbConfig = {
+  DB_HOST: process.env.DB_HOST!,
+  DB_PORT: Number(process.env.DB_PORT),
+  DB_NAME: process.env.DB_NAME!,
+  DB_USER: process.env.DB_USER!,
+  DB_PASSWORD: process.env.DB_PASSWORD!,
+};
+
+export const test = base.extend<
+  {},
+  {
+    dbAdapter: MySQLAdapter
+  }
+>({
+  dbAdapter: [
+    async ({}, use) => {
+      const db = new MySQLAdapter(dbConfig);
+      await db.connect();
+      await use(db);
+      await db.closeConnection();
+    }, { scope: 'worker' }],
+});
---
+++ tests/Screenplay/connect_database.spec.ts
@@ -0,0 +1,15 @@
+import{ expect } from "@playwright/test";
+import { test } from "@framework/fixtures/dbAdapter";
+import { Actor } from "@screenplay/core/Actor";
+import { AccessDatabase } from "@screenplay/abilities/AccessDatabase";
+import { ExecuteSqlScript } from "@screenplay/tasks/ExecuteSqlScript";
+import { DoesDataExist } from "@screenplay/questions/DoesDataExist";
+
+test.describe('Connect Database - Screenplay', () => {
+  test('Should Be Possible Connect Database', async ({ dbAdapter }) => {
+    const actor = new Actor('Tester').whoCan(AccessDatabase.using(dbAdapter));
+    const scriptResult = await actor.attemptsTo(ExecuteSqlScript.fromFile('src/sql/test.sql'))
+    const hasRows = await actor.asksFor(DoesDataExist.fromRows(scriptResult))
+    expect(hasRows).toBeTruthy();
+  });
+})
